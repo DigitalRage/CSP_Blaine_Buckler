9d2961b3e569a083c8bb8da3e457fa578596db58
DigitalRage <blaine.buckler@ucas-edu.net>
2025-01-29 09:53:07 -0700

    Python Practice

---
+++ Variables/practice.py
@@ -0,0 +1,22 @@
+# Blaine Buckler
+name = input("Blaine Buckler")
+
+one_to_ten = input("7")
+
+hundred_to_thousand = input("256")
+
+breakfast = input("Lucky Charms")
+
+color = input("Navy Blue")
+
+school = input("Utah Country Acadamy of Sciences")
+
+year = input("2025")
+
+eye_color = input("Dark Brown")
+
+age = input("14")
+
+favorite_food = input("Tacos and Ribs")
+
+print("My I'm" name+ )
