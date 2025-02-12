c8088713aeac26e6393c6eccb617437299544050
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-12 09:52:09 -0700

    Variables

--- Variables/practice.c
+++ Variables/practice.c
@@ -5,6 +5,6 @@ int num = 14;
 float pi = 3.14;  
 
 int main(void){
-    printf("Hello I'm", num "my age is %d and my favorite number is %f\n num", name);
+    printf("Hello I'm %s my age is %d and my favorite number is %f\n num", name);
     return 0;
 }
