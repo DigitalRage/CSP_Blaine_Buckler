dab004d48ec94230446c2a9d2f66277c9083d937
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-12 09:17:50 -0700

    Variabes Python

--- Variables/practice.py
+++ Variables/practice.py
@@ -20,5 +20,12 @@ age = "14"
 
 favorite_food = "Tacos and Ribs"
 
-print("Hi I'm", name+ " if I pick a number between 1-10 I would chose", one_to_ten)
-print("A number I would pick between 100-1000 would be", hundred_to_thousand)
+subject = "Digital Media"
+
+print("Hi I'm", name+ " if I pick a number between 1-10 I would chose", one_to_ten+ ".")
+print("A number I would pick between 100-1000 would be", hundred_to_thousand+ ".")
+print("What I had for breakfast today was", breakfast+ ".") 
+print("My favorite color is", color+ ".")
+print("The school I go to is", school+ ".")
+print("My age is", age+ ".")
+print("My favorite subject is", subject+ ".")
