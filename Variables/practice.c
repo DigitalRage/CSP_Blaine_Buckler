d2c4f48eab0cb3140997c55f5eb9a0ab437c3213
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-13 09:43:25 -0700

    C variables practice

--- Variables/practice.c
+++ Variables/practice.c
@@ -1,10 +1,15 @@
 #include <stdio.h> 
 
 char name[] = "Blaine"; 
-int num = 14;
-float pi = 3.14;  
+int age = 14;
+float pi = 3.14;
+int num = 7;
+int hun_to_thou = 256;
+char breakfast[] = "Lucky Charms"; 
+char color[] = "Navy Blue"; 
+char subject[] = "Digital Media"; 
 
 int main(void){
-    printf("Hello I'm %s my age is %d and my favorite number is %f\n num", name);
+    printf("Hello I'm %s my age is %d and my favorite number between one and ten is %d. \nIf i would chose a number between 100 and 1000 I would chose %d, what I had for breakfast was %s. \nMy favorite color is %s and favorite subject is %s. ", name, age, num, hun_to_thou, breakfast, color, subject);
     return 0;
 }
---
+++ strings/silly_sentences.c
@@ -0,0 +1,14 @@
+//Blaine Buckler, silly sentences c
+#include <stdio.h>
+//string variables for my user inputs (minimum of 3)
+
+int main(void){
+    
+    printf("What is your name?")
+    //introduce user to program. . .tell them what is heppening (print statement)
+
+    // create user inputs (print statements with questions AND scanf to collect the info)(same as the nuber of variables)
+
+    //insert variables into the sentence to show the user (print statement, only 1) example: "hello %s", name"
+    return 0; 
+}
