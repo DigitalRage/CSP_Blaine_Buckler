4e48c6ee2665dad50db39812da515618e6bd337c
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-14 09:53:06 -0700

    Started c first program

---
+++ Variables/first_program.c
@@ -0,0 +1,20 @@
+//Blaine Buckler, first program c
+#include <stdio.h>
+
+int main(void){
+    char name[50];
+    int num, num_two
+    printf("What is your name: "); 
+    scanf("%s", name); 
+
+    printf("Hello %s\n", name);
+
+    printf("Please give me a number: ");
+    scanf("%d", &num)
+
+    printf("Please give me a second number: "); 
+    scanf("%d", &num_two)
+
+    printf("Your numbers added together will be: "%d")
+    return 0; 
+}
