3eb09140a7bc05a68965d10c6c937869227fd4eb
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-16 23:39:22 +0000

    C first program done

--- Variables/first_program.c
+++ Variables/first_program.c
@@ -3,18 +3,18 @@
 
 int main(void){
     char name[50];
-    int num, num_two
+    int num, num_two;
     printf("What is your name: "); 
     scanf("%s", name); 
 
     printf("Hello %s\n", name);
 
     printf("Please give me a number: ");
-    scanf("%d", &num)
+    scanf("%d", &num);
 
     printf("Please give me a second number: "); 
-    scanf("%d", &num_two)
+    scanf("%d", &num_two);
 
-    printf("Your numbers added together will be: %d" num+num_two);
+    printf("Your numbers added together will be: %d\n", num+num_two);
     return 0; 
 }
