c7dd34e338e17768770822a8c1175f6fcfabc115
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-17 06:24:43 +0000

    Edits

--- Variables/first_program.py
+++ Variables/first_program.py
@@ -1,11 +1,11 @@
 #Blaine Buckler, first program python
 
-name = input("Please tell me your name").strip().capitalize()
+name = input("Please tell me your name: ").strip().capitalize()
 
 
 print(name+ ", welcome to my program")
 
-num = int(input("Write a number"))
-num_2 = int(input("Please write another number"))
+num = int(input("Write a number: "))
+num_2 = int(input("Please write another number: "))
 
 print("Your numbers will add up to", num+num_2,)
