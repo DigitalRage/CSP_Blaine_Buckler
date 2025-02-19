1c6d93c1939b8350a61027d1b46db23f07a8bdcc
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-19 09:54:31 -0700

    Commit

---
+++ Expressions/expressions.py
@@ -0,0 +1,44 @@
+#Blaine Buckler, Expressions Notes Python
+num = 7
+numTwo = 24
+numThree = 8
+numFour = 4
+numFive = 6
+
+print(int(num-numTwo/numThree*numFour+numFive))
+
+num = 18
+numTwo = 3
+numThree = 7
+numFour = 2
+numFive = 5
+
+print(int(num/numTwo-numThree+numFour*numFive))
+
+num = 6
+numTwo = 4
+numThree = 12
+numFour = 72
+numFive = 8
+numSix = 9
+
+print(int(num*numTwo/numThree+numFour/numFive-numSix))
+
+num = 17
+numTwo = 6
+numThree = 2
+numFour = 4
+numFive = 3
+
+print(int((num-numTwo/numThree)+numFour*numFive))
+
+num = -2
+numTwo = 1
+numThree = 4
+numFour = 2
+numFive = 2
+numSix = 6
+numSeven = 2
+numEight = 3
+
+print(int(num*(numTwo*numThree-numFour/numFive)+(numSix+numSeven-numEight)))
---
+++ Expressions/notes.py
@@ -0,0 +1,15 @@
+#Blaine Buckler, Expressions Notes Python
+num = 4
+numTwo = 8
+numThree = 10
+
+print(num+numTwo)
+print(num-numTwo)
+print(num*numTwo)
+print(int(num/numTwo))
+print(num**numTwo)
+print(num%numTwo)
+
+num = num - numTwo
+
+print(num)
