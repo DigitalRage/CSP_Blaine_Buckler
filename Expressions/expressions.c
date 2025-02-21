1a854e53f9739f40ed4b4136d41fca03d2923ce0
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-21 17:46:30 +0000

    More Work

--- Expressions/expressions.c
+++ Expressions/expressions.c
@@ -2,23 +2,40 @@
 #include <stdio.h> 
 #include<math.h>
 int main(void){
-    int num = 7; 
+
+    int exponentOfTwo = 2; 
+    int exponentOfThree = 3; 
+    int exponentOfFour = 4; 
+    int exponentOfFive = 5; 
+
+    int numOne = 7; 
     int numTwo = 24; 
     int numThree = 8; 
     int numFour = 4; 
     int numFive = 6; 
 
-    int result = num - numTwo / numThree * numFour + numFive; 
+    int result = numOne - numTwo / numThree * numFour + numFive; 
 
-    printf("%d", result); 
+    printf("%d\n", result); 
 
-    int numOne = 18; 
+    int numOneTwo = 18; 
     int numTwoOne = 3; 
     int numThreeOne = 7; 
     int numFourOne = 2; 
     int numFiveOne = 5; 
 
-    int resultTwo = numOne - numTwoOne / numThreeOne * numFourOne + numFiveOne; 
+    int resultTwo = numOneTwo / numTwoOne - numThreeOne + numFourOne * numFiveOne; 
 
-    printf("%d", resultTwo); 
+    printf("%d\n", resultTwo); 
+
+    int numOneThree = 6; 
+    int numTwoThree = 4; 
+    int numThreeThree = 12; 
+    int numFourThree = 72; 
+    int numFiveThree = 8;
+    int numSixThree = 9;  
+
+    int resultThree = numOneThree * numTwoThree - numThreeThree + numFourThree / numFiveThree - numSixThree;
+
+    printf("%d\n", resultThree);  
 }
