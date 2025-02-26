3a4b8b6171acfa5fe844392aa29a608bd7aae443
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-26 09:51:50 -0700

    Work on Calculator

---
+++ Expressions/financialCalculator.py
@@ -0,0 +1,11 @@
+# Blaine Buckler, Financial Calculator C
+print("Welcome to my program, this program will calculate your finances. ")
+
+income = input("Please tell me your monthly income: ")
+
+rent = input("Please tell me your rent or morgage: ")
+utilities = input("Please tell me your utility bill: ")
+food = input("Please tell me your cost for groceries: ")
+Tranport = input("Please tell me your typical cost for transportation: ")
+
+cashLeft = print("You left over income is", ()
