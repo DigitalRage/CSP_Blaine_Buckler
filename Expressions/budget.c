c42122191bf33d6e697833b9bb2b0da3d510a6e0
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-27 17:12:47 +0000

    work

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 int main(void){
-    flost income, remt, utilities, groceries, transportation, savings, expenses, total; 
-    printf("Hello World"); 
+    float income, remt, utilities, groceries, transportation, savings, expenses, total; 
+    printf("Hello and welcome to our budget calculator\n"); 
+    printf("How much do you make each month?");
+    scanf("%f", &income); 
+    printf("Your monthly income is $%.2f\n", income); 
     return 0;
 }
