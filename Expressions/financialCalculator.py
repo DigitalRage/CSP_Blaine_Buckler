99b39f4b5a6be5c18552eba807d6db2e58c1cac7
DigitalRage <blaine.buckler@ucas-edu.net>
2025-02-28 09:53:15 -0700

    work

--- Expressions/financialCalculator.py
+++ Expressions/financialCalculator.py
@@ -9,12 +9,28 @@ spending = int(input("Please tell me how much you spend on extra stuff a month: 
 food = int(input("Please tell me your monthly groceries bill: \n"))
 transport = int(input("Please tell me your monthly transport bill: \n"))
 
-prent = rent/income
-putilities = utilities/income
-pspending = spending/income
-pfood = food/income
+total = income-rent-utilities-spending-food-transport
 
-leftOver = income - rent - utilities - food - transport
+prent = rent/income*100
+putilities = utilities/income*100
+pspending = spending/income*100
+pfood = food/income*100
+ptransort = transport/income*100
+
+ptotal = prent+putilities+pspending+pfood+ptransort
+
+psavings = (100-(prent+putilities+pspending+pfood+ptransort))*0.2
+
+print("your total spendings is", ptotal, "% of your oncome")
+print("Your rent is", "{:.2f}".format(prent), "% of your income\n")
+print("Your utilities are", "{:.2f}".format(putilities), "% of your income\n")
+print("Your spendings are", "{:.2f}".format(pspending), "%  of your income\n")
+print("Your groceries is", "{:.2f}".format(pfood), "% of your income\n")
+print("Your pay for transportation is", "{:.2f}".format(ptransort), "% of your income\n")
+print("You should put", "{:.2f}".format(psavings), "% into your savings")
+
+leftOver = income - (rent + utilities + food + transport + psavings)
 
 
-print("Your leftover income is", leftOver)
+print("Your leftover income would be", leftOver*0.1, "%\n")
+print("In actual cash you will have", total, "dollars left. \n")
