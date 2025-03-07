70d50392b2582d9e4d2b737cf3c84514d58ea9c3
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-07 09:51:12 -0700

    Work

--- Expressions/financialCalculator.py
+++ Expressions/financialCalculator.py
@@ -5,31 +5,29 @@ print("Welcome to my program, this program will calculate your finances. \n")
 income = float(input("Please tell me your monthly income: \n$"))
 rent = float(input("Please tell me your monthly rent: \n$"))
 utilities = float(input("Please tell me your monthly utilities: \n$"))
-spending = float(input("Please tell me how much you spend on extra stuff a month: \n$"))
 food = float(input("Please tell me your monthly groceries bill: \n$"))
 transport = float(input("Please tell me your monthly transport bill: \n$"))
 
-total = income - rent - utilities - spending - food - transport
+total = income - rent - utilities - food - transport
 
 prent = rent / income * 100
 putilities = utilities / income * 100
-pspending = spending / income * 100
 pfood = food / income * 100
 ptransport = transport / income * 100
 
-ptotal = prent + putilities + pspending + pfood + ptransport
+ptotal = prent + putilities + pfood + ptransport
 
-psavings = (100 - (prent + putilities + pspending + pfood + ptransport)) * 0.2
+savings = income * 0.1
 
 print("\nYour total spendings is", "{:.2f}".format(ptotal) + "% of your income\n")
 print("Your rent is", "{:.2f}".format(prent) + "% of your income\n")
 print("Your utilities are", "{:.2f}".format(putilities) + "% of your income\n")
-print("Your spendings are", "{:.2f}".format(pspending) + "% of your income\n")
 print("Your groceries is", "{:.2f}".format(pfood) + "% of your income\n")
 print("Your pay for transportation is", "{:.2f}".format(ptransport) + "% of your income\n")
-print("You should put", "{:.2f}".format(psavings) + "% into your savings")
+print("You should put", "{:.2f}".format(savings) + "$ into your savings\n")
 
-leftOver = 100 - (ptotal + psavings)
+spending = (total / income * 100) -10
+pspending = total * 0.9
 
-print("Your leftover income would be", "{:.2f}".format(leftOver) + "%\n")
-print("In actual cash you will have $" + "{:.2f}".format(total) + ". \n")
+print("You you will have", "{:.2f}".format(spending) + "% left to spend\n")
+print("In actual cash you will have $" + "{:.2f}".format(pspending) + ". \n")
--- Strings/notes.py
+++ Strings/notes.py
@@ -27,3 +27,20 @@ word = sentence.find("fox")
 #print(f"Welcome to my program {name}!")
 percent = 89
 print(f"your grade is {percent:.1f}")
+
+#1. Words in programing (data type)"" ''
+#2. Why do we have strings?
+#3. How do stupid proofing and sanitization relate to each other?
+#4. What is debugging?
+#5. How do you debug the different types of errors?
+#6. Describe what each of the methods below does:
+# find()
+# concatenate (add)
+# upper()
+#lower()
+#strip()
+#7. How are strings in C different from python? 
+#8. What keyword is used to set a string?
+#9. What do we need to add to the print statement to make a string variable print?
+#10. Use strcat()
+#11. How do you change a string in C?
