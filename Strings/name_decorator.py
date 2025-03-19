e9e54dd3630975ffcc54688520082013402494c8
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-19 22:04:50 +0000

    Work

--- Strings/name_decorator.py
+++ Strings/name_decorator.py
@@ -1,8 +1,12 @@
 # Blaine Buckler Name decorator, Python
 
-name = input("Please tell me your name: ")
+name = input("Please tell me your name (first leter of each name is a capital): ")
 
-print("<><><>"+name+"<><><>")
+print("\n<><><>"+name+"<><><>")
 print("._."+name+"._.")
-print("<><><>"+name+"<><><>")
-print("<><><>"+name+"<><><>")
+print("It's "+name+" the fantastic! ")
+print("OwO "+name+" ^w^")
+print("#"+name+" is cool")
+print("Message me at "+name+"@gmail.com")
+print("$$$"+name+"$$$")
+print("|#1| "+name+" |#1|")
