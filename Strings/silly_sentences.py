2b82433a64fe4fa1de312ff337012f6dd3d39d58
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-19 22:55:24 +0000

    Work

---
+++ Strings/silly_sentences.py
@@ -0,0 +1,11 @@
+#Blaine Buckler, silly sentences, Python
+name = input("What is your name? ")
+
+print("This is like a MadLib\nPlease answer the questions. ")
+
+verb = input("Please tell me a verb: ")
+noun = input("Please tell me a noun: ")
+adverb = input("Please tell me a adverb: ")
+
+sentence = "\nYou had a nice day"+verb+"ing on the sidewalk; suddenly a "+noun+' caught you off gard, "you '+ adverb + " mangy"+noun+ '" you replied.' 
+print(sentence)
