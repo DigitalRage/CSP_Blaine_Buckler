831421367b921d81ba05f187490b4a34467a303b
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-19 23:48:22 +0000

    Work

--- Strings/silly_sentences.c
+++ Strings/silly_sentences.c
@@ -1,24 +1,40 @@
 //Blaine Buckler, silly sentences c
 #include <stdio.h>
-//string variables for my user inputs (minimum of 3)
 
-// Blaine Buckler Name decorator, C
-#include <stdio.h>
+#include <string.h>
 
 int main(void){
-    char name[50]; 
-    printf("Please tell me your name: "); 
-    scanf("%s", name);
-    printf("Hello %s this is like a MadLib\nPlease answer the questions.\n", name); 
+    char sentence[200] = "As a "; //all strings for the sentence end up here in the end
+    char name[50];
+    char noun[50];
+    char adverb[50];
+    char verb[50];
 
-    printf("Please tell me a noun")
-    printf("Please tell me a adjetive")
-    printf("Please tell me a ")
+    printf("Please tell me your name: \n"); 
+    scanf("%s", name);
+    printf("Hello %s this is like a MadLib\nPlease answer the questions.\n", name);
+
+    printf("Please tell me a noun: ");
+    scanf("%s", noun);
+
+    printf("Please tell me a adverb: ");
+    scanf("%s", adverb);
+
+    printf("Please tell me a past tense verb: ");
+    scanf("%s", verb);
+
+    strcat(sentence, noun);
+    strcat(sentence, " ");
+    strcat(sentence, adverb); 
+    strcat(sentence, " walked by another "); 
+    strcat(sentence, noun); 
+    strcat(sentence, " it "); 
+    strcat(sentence, adverb);
+    strcat(sentence, " back ");
+    strcat(sentence, adverb);
+    strcat(sentence, ". ");
+
+    printf("%s", sentence); 
 
     return 0;
 }
-    //introduce user to program. . .tell them what is heppening (print statement)
-
-    // create user inputs (print statements with questions AND scanf to collect the info)(same as the nuber of variables)
-
-    //insert variables into the sentence to show the user (print statement, only 1) example: "hello %s", name"
