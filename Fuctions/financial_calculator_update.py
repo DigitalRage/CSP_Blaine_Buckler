5b4ad995b4c2cc0db46d203eac86b56b453a2db6
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-21 16:23:23 +0000

    Work

--- Fuctions/financial_calculator_update.py
+++ Fuctions/financial_calculator_update.py
@@ -3,38 +3,50 @@
 print("Welcome to my program, this program will calculate your finances. \n")
 
 
-income = float(input("Please tell me your monthly income: \n$"))
-rent = float(input("Please tell me your monthly rent: \n$"))
-utilities = float(input("Please tell me your monthly utilities: \n$"))
-food = float(input("Please tell me your monthly groceries bill: \n$"))
-transport = float(input("Please tell me your monthly transport bill: \n$"))
+def get_inputs():
+
+    income = float(input("Please tell me your monthly income: \n$"))
+    rent = float(input("Please tell me your monthly rent: \n$"))
+    utilities = float(input("Please tell me your monthly utilities: \n$"))
+    food = float(input("Please tell me your monthly groceries bill: \n$"))
+    transport = float(input("Please tell me your monthly transport bill: \n$"))
+    return income, rent, utilities, food, transport
+
+income, rent, utilities, food, transport = get_inputs()
+
 savings = income * 0.2
 exspenses = rent + utilities + food + transport
 spending = income - exspenses - savings
-#prent = rent / income * 100
-#putilities = utilities / income * 100
-#pfood = food / income * 100
-#ptransport = transport / income * 100
-#psavings = savings/income * 100
-#pexspenses = exspenses/income * 100
+prent = rent / income * 100
+putilities = utilities / income * 100
+pfood = food / income * 100
+ptransport = transport / income * 100
+psavings = savings/income * 100
 def percent(type, amount): 
     per = amount/income *100
 
-    print(f"Your {type} is {per:.2f}% income. ")
+    print(f"Your {type} is {per:.2f}% income. \n")
 
-print(f"Your monthly income is ${income:.2f}\n")
-print(f"Your monthly exspenses are ${exspenses:.2f}\n")
-print(f"Your monthly savings is ${savings:.2f}\n")
-print(f"Your monthly spending money is ${spending:.2f}\n")
+def cash(type, amount): 
+
+    print(f"Your monthly {type} is ${amount:.2f}\n")
+#print(f"Your monthly income is ${income:.2f}\n")
+#print(f"Your monthly exspenses are ${exspenses:.2f}\n")
+#print(f"Your monthly savings is ${savings:.2f}\n")
+#print(f"Your monthly spending money is ${spending:.2f}\n")
+cash("rent", rent)
+cash("exspenses", exspenses)
+cash("savings", savings)
+cash("spending", spending)
 #print(f"Your rent is %{int(prent)} of your monthly income\n")
 #print(f"Your utilities are {int(putilities)}% of your income\n")
 #print(f"Your groceries are {int(pfood)}% of your income\n")
 #print(f"Your transportation are {int(ptransport)}% of your income\n")
 #print(f"Your savings are {int(psavings)}% of your income\n")
 #print(f"Your exspenses are {int(pexspenses)}% of your income\n")
-print(percent("rent", rent))
-print(percent("utilities", utilities))
-print(percent("groceries", food))
-print(percent("transportation", transport))
-print(percent("savings", savings))
-print(percent("exspenses", exspenses))
+percent("rent", rent)
+percent("utilities", utilities)
+percent("groceries", food)
+percent("transportation", transport)
+percent("savings", savings)
+percent("exspenses", exspenses)
