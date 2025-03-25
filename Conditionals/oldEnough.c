1d0bce6908736abcfb7118d917d214338c4ede53
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-25 00:00:46 +0000

    Work

--- Conditionals/oldEnough.c
+++ Conditionals/oldEnough.c
@@ -1,40 +1,22 @@
 // Blaine Buckler, Old Enough C
 #include <stdio.h>
 #include <string.h> 
-int age
 
 int main(void){
-    if(age > 18){
-        printf("You are old enough to drive. ")
-    }else if (age >= 15){
-        printf("You are old enough to have a drivers permit. "); 
-    }else{
-        printf("You are not old enoug"); 
-    }
+    int age; 
+    printf("Please tell me your age: "); 
+    scanf("%d", &age); 
 
+    if(age >= 18){
+        printf("You are old enough to vote. \n"); 
+    }else if (age >= 16){
+        printf("You are old enough to drive. \n"); 
+    }else if (age >= 15){
+        printf("You are old enough to have a drivers permit. \n"); 
+    }else if (age >= 5){
+        printf("You are old enough to go to school. \n"); 
+    }else{
+        printf("You are not old enough to go to school. \n"); 
+    }
     return 0;
 } 
-
-age = int
-printf("Please tell me your age: "); 
-scanf("%d", age); 
-
-if age < 128: 
-    if age >= 18:
-        print("You are old enough to vote.\n"); 
-    else:
-        print("You are not old enough to vote.\n"); 
-
-    if age >= 16:
-        print("You are old enough to drive.\n"); 
-    elif age >= 15:
-        print("You are old enough to have a driver's permit.\n"); 
-    else:
-        print("You are not old enough to drive.\n"); 
-
-    if age >= 5:
-        print("You are old enough to go to school.\n"); 
-    else:
-        print("You are not old enough to go to school.\n"); 
-else: 
-    print("How are you alive?"); 
