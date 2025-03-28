0bdf482fab4320a4e3b8df9dbad14f337f2c7e68
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-28 16:50:37 +0000

    Work

--- loops/myFamilyLoop.c
+++ loops/myFamilyLoop.c
@@ -1,14 +1,20 @@
-// Blaine Buckler, My Family Loop C
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
+int main(void) {
     printf("These are some of my family members: "); 
-    char famMembers[][20] ={"Sam, ", "Bruce, ", "Kitsune, ", "Meika, ", "Fletcher, ", "Christina"}; 
+
+    char famMembers[][20] = {"Sam, ", "Bruce, ", "Kitsune, ", "Meika, ", "Fletcher, ", "Christina"};
+    
+    strcpy(famMembers[5], " and Christina."); 
+
     int m = 0; 
-    int mlength = sizeof(famMembers)/sizeof(famMembers[0]); 
-    while(m < mlength){
-        famMembers[5] = " and Christina. "
+    int mlength = sizeof(famMembers) / sizeof(famMembers[0]); 
+
+    while (m < mlength) {
         printf("%s", famMembers[m]); 
         m++; 
     }
+
+    return 0;
 }
