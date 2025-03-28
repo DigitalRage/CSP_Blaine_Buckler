7ee21454d6e5aafd7754f79fe5e40453c7c532cf
DigitalRage <blaine.buckler@ucas-edu.net>
2025-03-28 16:41:07 +0000

    Work

--- loops/fizzBuzz.py
+++ loops/fizzBuzz.py
@@ -1,1 +1,15 @@
 #Blaine Buckler, Fizz Buzz Python
+
+num = 1
+multiplier = 3  # Set any number for the multiplier
+multiplierT = 5
+while num < 51:
+    if num % multiplier == 0 and num % multiplierT == 0: 
+        print("FizzBuzz")
+    elif num % multiplier == 0:  # Check if 'num' is a multiple of 'multiplier'
+        print("Fizz")
+    elif num % multiplierT == 0: 
+        print("Buzz")
+    else:
+        print(num)
+    num += 1
