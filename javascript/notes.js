7ec943dc8061ea16bb8d6a61900cb119d6e2a045
DigitalRage <blaine.buckler@ucas-edu.net>
2025-04-24 09:53:28 -0600

    Work

--- javascript/notes.js
+++ javascript/notes.js
@@ -25,3 +25,13 @@ function normal(){
 function show(){
     document.getElementById("hidden").style.display = "block"
 }
+
+function more(){
+    if(document.getElementById("extra").style.display != "flex"){
+    document.getElementById("extra").style.display = "flex" 
+    document.getElementById("shw").innerHTML = "Show Less"
+    }else{
+        document.getElementById("extra").style.display = "none"
+    document.getElementById("shw").innerHTML = "Show More"
+    }
+}
