547fa7889dd614493b2b5abce68c10f8924b4bbd
DigitalRage <blaine.buckler@ucas-edu.net>
2025-04-29 09:37:11 -0600

    Work

--- staycation/staycation.js
+++ staycation/staycation.js
@@ -1,15 +1,20 @@
-let holNRok = [
-    "https://southwestcontemporary.com/wp-content/uploads/2023/10/IMG_0426.jpg",
-    "https://milliverstravels.com/wp-content/images/Hole-in-the-Rock-arrow.jpg","https://southwestcontemporary.com/wp-content/uploads/2023/10/IMG_0351.jpg"
-];
-let count = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    let holNRok = [
+        "https://southwestcontemporary.com/wp-content/uploads/2023/10/IMG_0426.jpg",
+        "https://milliverstravels.com/wp-content/images/Hole-in-the-Rock-arrow.jpg",
+        "https://southwestcontemporary.com/wp-content/uploads/2023/10/IMG_0351.jpg", 
+        "https://i.ytimg.com/vi/CLNhlnRh8nQ/maxresdefault.jpg"
+    ];
+    let count = 0;
 
-function change() {
-    count += 1; 
-    if (count >= holNRok.length) { 
-        count = 0;
+    function change() {
+        count += 1; 
+        if (count >= holNRok.length) { 
+            count = 0;
+        }
+        document.getElementById("rok").src = holNRok[count]; 
     }
-    document.getElementById("Images").src = holNRok[count]; 
-}
 
-setInterval(change, 3000); 
+    setInterval(change, 3000);
+});
+
